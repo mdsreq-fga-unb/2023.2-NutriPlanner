d233f3bc8f15fefde6bce111df00b4c3bffaba3e
MillenaQueiroz <millena.queiroz08@gmail.com>
2023-11-26 00:14:35 -0300

    feat(login): Add componente e rota

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,1 +1,21 @@
 const express = require('express');
+
+const routes = express.Router();
+
+const users =[{
+    id:1,
+    name: 'Admin',
+    pin: '12345'
+}];
+
+routes.post('/login', (req,res) =>{
+    const {pin} = req.body;
+    const user = users.find(user => user.pin == pin);
+    if(user){
+        return res.status(200).json(user);
+    }
+    return res.status(401).json({message: 'Pin inválido'});
+
+});
+
+module.exports = routes;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
 app.get('/', (req,res) =>{
     res.send('Hello World!');

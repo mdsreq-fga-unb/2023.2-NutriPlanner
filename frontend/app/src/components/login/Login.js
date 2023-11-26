74b8f410f7664acfaa74e87caf711b9ae8031d80
MillenaQueiroz <millena.queiroz08@gmail.com>
2023-11-26 10:44:28 -0300

    feat(login): adiciona funçao de login no componente

--- frontend/app/src/components/login/Login.js
+++ frontend/app/src/components/login/Login.js
@@ -1,15 +1,34 @@
+import { useState } from "react";
+import axios from 'axios';
+
 function Login() {
+
+    const [pin, setPin] = useState('');
 
     const handleLogin = async(e) => {
         e.preventDefault();
-        console.log('teste quero dormir');
+        console.log(pin);
+
+        const response = await axios.post('http://localhost:3000/login', 
+            JSON.stringify({pin}),
+            {
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
     }
 
     return (
         <div className="login-form-wrap">
             <h2>Bem Vindo</h2>
             <form className="login-form">
-                <input type="pin" name="pin" placeholder="pin" required />
+                <input type="pin" 
+                        name="pin" 
+                        placeholder="pin" 
+                        required
+                        onChange={(e) => setPin(e.target.value)}
+                        />
             </form>
             <button type="submit" 
                     className="btn-login"

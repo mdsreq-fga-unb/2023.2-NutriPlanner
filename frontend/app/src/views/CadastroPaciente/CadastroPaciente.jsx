1d30ed485ae1993511c6fa900efe5421701a1d7b
Lucas Spinosa <lucasspinosa.software@gmail.com>
2023-11-29 18:03:40 -0300

    (feat)(frontend): componentes da pagina de cadastro

---
+++ frontend/app/src/views/CadastroPaciente/CadastroPaciente.jsx
@@ -0,0 +1,15 @@
+import './CadastroPaciente.css'
+import React from 'react'
+
+import Logo from '../../components/Logo/Logo'
+import Nav from '../../components/Nav/Nav'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+export default props =>
+    <div className="cadastro">
+        <Logo />
+        <Nav />
+        <Header />
+        <Footer />
+    </div>

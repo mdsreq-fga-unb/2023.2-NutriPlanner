466da531d6a4293961f117d46cabf4bd2a798b2b
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2023-12-03 02:35:43 -0300

    feat(aplicacao): inclusão do CORS na aplicação

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors")
 
 const pacienteRoutes = require("./routes/pacienteRoutes.js");
 
 dotenv.config();
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.use(cors());
 
 const port = process.env.PORT || 3000
 

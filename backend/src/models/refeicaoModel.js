cd56f20896654f237186e49cdd383beaccb31fc3
Leandro Silva <leanddrosouzasilva@gmail.com>
2023-12-06 00:39:38 -0300

    Criado os models

--- backend/src/models/refeicaoModel.js
+++ backend/src/models/refeicaoModel.js
@@ -4,6 +4,6 @@ const refeicoesSchema = new mongoose.Schema({
     nome: String
 });
 
-const Refeicao = mongoose.model('Refeição', refeicoesSchema, 'Refeições')
+const Refeicao = mongoose.model('Refeicao', refeicoesSchema, 'Refeicoes')
 
 module.exports = Refeicao
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.json());
 
+
+
 app.get('/', (req,res) =>{
     res.send('Hello World!');
 });

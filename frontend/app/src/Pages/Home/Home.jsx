1521fc22af3ee2aa3a6800c8630ca72b7f8c41e6
Lucas Spinosa <lucasspinosa.software@gmail.com>
2023-12-09 23:32:35 -0300

    feat (home): inserção de aniversariantes na página

--- frontend/app/src/Pages/Home/Home.jsx
+++ frontend/app/src/Pages/Home/Home.jsx
@@ -25,15 +25,13 @@ import grupoPacientes from '../../assets/icons/GrupoPacientes.svg';
 import consultas from '../../assets/icons/Consultas.svg';
 import bolo from '../../assets/icons/Bolo de Aniversário.svg';
 
-let quantidades;
-
 function buscaQuantidadePacientes(){
-    const url = 'http://localhost:3000/pacientes';
+    const url = 'http://localhost:3000/pacientes/buscaQuantidades';
 
     
     axios.get(url)
     .then((response) => {
-        quantidades = response.data.dados;
+        const quantidades = response.data.dados;
 
         const cardPacientes = document.getElementById('quantidadesPacientes');
         
@@ -50,11 +48,48 @@ function buscaQuantidadePacientes(){
 
         cardPacientes.innerHTML = `<p class="Card-Pacientes-item">Não foi possível obter a quantidade de pacientes.</p>`
       });
+};
 
+function buscaAniversariantes(){
+    const url = 'http://localhost:3000/pacientes/buscaAniversarios';
+
+    axios.get(url)
+    .then((response) => {
+        const aniversariantes = response.data.data.aniversariantes;
+        const dataAtual = new Date();
+
+        const areaAniversarios = document.getElementById('pacientesAniversariantes');
+
+        for(let i = 0; i < aniversariantes.length; i++){
+            // Card de aniversário
+            const cardAniversario = document.createElement('div');
+
+            cardAniversario.classList.add('Aniversariantes-paciente');
+
+            const idadePaciente = aniversariantes[i].dtNascimento.get
+
+            cardAniversario.innerHTML = 
+            `
+            <div class="Aniversariante-foto"></div>
+            <div class='Aniversariante-dados'>
+                <span class='Aniversariante-texto'>${aniversariantes[i].nome}</span>
+                <span class='Aniversariante-texto'>${aniversariantes[i].dtNascimento}</span>
+                <span class='Aniversariante-texto'>Fará 26 anos!</span>
+                <a class='Aniversariante-link' href="">Ver dados do Paciente</a>
+            </div>
+
+            `
+
+            areaAniversarios.appendChild(cardAniversario);
+        }
+      }, (error) => {
+        console.log(error.response)
+      });
 };
 
 const Home = () =>{
     buscaQuantidadePacientes();
+    buscaAniversariantes();
 
     return(     
         <div className="Home">
@@ -115,20 +150,10 @@ const Home = () =>{
                     </div>
                 </div>
                 <hr className="Home-divisao-conteudo"></hr>
-                <div className='Home-aniversariantes'>
+                <div className='Home-aniversariantes' id="pacientesAniversariantes">
                     <div className='Home-aniversariantes-cabecalho'>
                         <img className='Aniversariantes-cabecalho-imagem' src={bolo} alt="" />
                         <span className='Aniversariantes-cabecalho-texto'>Aniversariantes</span>
-                    </div>
-                    <div className="Aniversariantes-paciente">
-                        <div className="Aniversariante-foto">
-                        </div>
-                        <div className='Aniversariante-dados'>
-                            <span className='Aniversariante-texto'>Nome</span>
-                            <span className='Aniversariante-texto'>Aniversário</span>
-                            <span className='Aniversariante-texto'>Fará 26 anos!</span>
-                        </div>
-                            <a className='Aniversariante-link' href="">Ver dados do Paciente</a>
                     </div>
                 </div>
             </div>

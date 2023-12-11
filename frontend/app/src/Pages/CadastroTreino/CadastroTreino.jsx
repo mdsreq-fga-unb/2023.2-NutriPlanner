a0ab099edb01f5e601a43a4295fc841469150210
MillenaQueiroz <millena.queiroz08@gmail.com>
2023-12-11 01:24:52 -0300

    feat(criartreino): adiciona logica criar mais um treino

--- frontend/app/src/Pages/CadastroTreino/CadastroTreino.jsx
+++ frontend/app/src/Pages/CadastroTreino/CadastroTreino.jsx
@@ -24,11 +24,12 @@ import salvar from '../../assets/icons/Salvar.svg';
 import voltar from '../../assets/icons/Voltar.svg';
 
 const CadastroTreino = () =>{
+  
   const dadosTreino = {
     idPaciente: '656fe262751bb37143d35513',
     sessoes: [
       {
-        codigo: 'AA',
+        codigo: 'a',
         dia: '',
         regiaoFocal: '',
         duracaoAerobico: 0,
@@ -37,175 +38,26 @@ const CadastroTreino = () =>{
       },
     ],
   }
-
+  const [quantidadeTreinos, setQuantidadeTreinos] = useState(1);
+  const [indiceSessao, setIndiceSessao] = useState(0);
   let indiceExercicio = 0;
-  let indiceSessao = 0;
+  
 
-  const preencheAtributoExercicio = (event, indice) => {
-    const { name, value } = event.target;
-    
-    const exercicioAtual = dadosTreino.sessoes[0].exercicios[indice]
-    exercicioAtual[name] = value;
-};
-
-  function adicionarExercicio(event, index) {
-    event.preventDefault()
-
-    dadosTreino.sessoes[indiceSessao].exercicios.push({
-      nome: '',
-      series: 0,
-      repeticoes: 0,
-      carga: 0,
-      tecnicaAvancada: '',
-  })
-
-  let containerCampos = document.createElement('div')
-  containerCampos.classList.add('CadastroTreino-campos-exercicio');
-  containerCampos.dataset.index = indiceExercicio;
-
-    containerCampos.innerHTML = `
-        <div class="CadastroTreino-item-formulario-exercicio">
-            <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="nomeExercicio">Nome do Exercício</label>
-            <input class="CadastroTreino-input CadastroTreino-input-exercicio" type="text" id="nomeExercicio" name="nome" required="required" maxLength="200"/>
-        </div>
-        <div class="CadastroTreino-item-formulario-exercicio">
-            <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="series">Séries</label>
-            <input class="CadastroTreino-input CadastroTreino-input-series" type="tel" id="series" name="series" required="required" max="10" min="1" defaultValue="1"/>
-        </div>
-        <div class="CadastroTreino-item-formulario-exercicio">
-            <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="repeticoes">Repetições</label>
-            <input class="CadastroTreino-input CadastroTreino-input-repeticoes" type="tel" id="repeticoes" name="repeticoes" required="required" max="200" min="1" defaultValue="10"/>
-        </div>
-        <div class="CadastroTreino-item-formulario-exercicio">
-          <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="carga">Carga (kg)</label>
-          <input class="CadastroTreino-input CadastroTreino-input-carga" type="tel" id="carga" name="carga" required="required" max="200" min="1" defaultValue="10"/>
-        </div>
-        <div class="CadastroTreino-item-formulario-exercicio">
-          <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="carga">Técnica avançada</label>
-          <input class="CadastroTreino-input CadastroTreino-input-tecavancada" type="text" id="tecavancada" name="tecnicaAvancada" required="required" max="200" min="1" defaultValue="10"/>
-        </div>
-
-    `;
-
-    const inputNome = containerCampos.querySelector('.CadastroTreino-input-exercicio');
-    inputNome.addEventListener('change', function(e) {
-        preencheAtributoExercicio(e, containerCampos.dataset.index)
-    })
-
-    const inputSeries = containerCampos.querySelector('.CadastroTreino-input-series');
-    inputSeries.addEventListener('change', function(e) {
-        preencheAtributoExercicio(e, containerCampos.dataset.index)
-    })
-
-    const inputRepeticoes = containerCampos.querySelector('.CadastroTreino-input-repeticoes');
-    inputRepeticoes.addEventListener('change', function(e) {
-        preencheAtributoExercicio(e, containerCampos.dataset.index)
-    })
-
-    const inputCarga = containerCampos.querySelector('.CadastroTreino-input-carga');
-    inputCarga.addEventListener('change', function(e) {
-        preencheAtributoExercicio(e, containerCampos.dataset.index)
-    })
-
-    const inputTecAvancada = containerCampos.querySelector('.CadastroTreino-input-tecavancada');
-    inputTecAvancada.addEventListener('change', function(e) {
-        preencheAtributoExercicio(e, containerCampos.dataset.index)
-    })
-
-    document.getElementById('formExercicio').appendChild(containerCampos);
-    indiceExercicio++;
-  }
-
-  function removerExercicio(event) {
-    event.preventDefault()
-
-    dadosTreino.sessoes[indiceSessao].exercicios.pop()
-    const formExercicio = document.getElementById('formExercicio');
-    const exercicios = formExercicio.querySelectorAll('.CadastroTreino-campos-exercicio');
-
-    if (exercicios.length > 0) {
-        formExercicio.removeChild(exercicios[exercicios.length - 1]);
-    }
-
-    indiceExercicio--;
-  } 
-
-  function cadastrarTreino(event){
-    event.preventDefault();
-
-    const url = 'http://localhost:3000/treinos/656fe262751bb37143d35513';
-    console.log(dadosTreino)
-
-    axios.post(url, dadosTreino)
-      .then((response) => {
-        alert('Treino Cadastrado com sucesso!')
-      }, (error) => {
-        alert('Não foi possível adicionar o treino. Verifique os dados informados!')
-        console.log(error.response.data)
-      });
+  const handleChangeQuantidadeTreinos = (event) => {
+    const valor = parseInt(event.target.value, 10);
+    setQuantidadeTreinos(isNaN(valor) ? 1 : valor);
+    setIndiceSessao(isNaN(valor) ? 0 : valor - 1);
   };
 
-  const preencheAtributoTreino = (event) => {
-    let { name, value } = event.target;
+  const gerarDivsTreino = () => {
+    const divsTreino = [];
 
-    dadosTreino.sessoes[0][name] = value;
-};
-
-
-const diasDaSemana = [
-  'Domingo',
-  'Segunda-Feira',
-  'Terça-Feira',
-  'Quarta-Feira',
-  'Quinta-Feira',
-  'Sexta-Feira',
-  'Sábado',
-];
-
-    return(     
-        
-        <div className="CadastroPaciente">
-            <div className="CadastroPaciente-menu-lateral">
-                <Logo />
-                <span className="CadastroPaciente-menu-titulo">MENU</span>
-                <hr className="CadastroPaciente-menu-divisao" />
-                <nav className="CadastroPaciente-navegacao">
-                    <a href="/" className='CadastroPaciente-item-menu'>
-                        <MenuButton title="Pacientes" icon={paciente}/>
-                    </a>
-                    <a href="/" className='CadastroPaciente-item-menu'>
-                        <MenuButton title="Cadastrar Paciente" icon={adicaoPaciente}selecionado="true" />
-                    </a>
-                    <a href="/" className='CadastroPaciente-item-menu'>
-                        <MenuButton title="Criar Dieta" icon={dieta}/>
-                    </a>
-                    <a href="/cadastroTreino" className='CadastroPaciente-item-menu'>
-                        <MenuButton title="Criar Plano de Treino" icon={treino}/>
-                    </a>
-                    <a href="/" className='CadastroPaciente-item-menu'>
-                        <MenuButton title="Agendar Consulta" icon={agendamento} />
-                    </a>
-                </nav>
-            </div>
-    
-            <div className="CadastroPaciente-conteudo">
-                <div className="CadastroPaciente-cabecalho">
-                    <div className='CadastroPaciente-items-cabecalho'>
-                        <Header title="Criar Plano de Treino" caminhoImagem={adicaoPaciente} />
-                        <div className='CadastroPaciente-botoes-cabecalho'>
-                            <Button title="Ajuda" classeAdicional="CadastroPaciente-botoes-cabecalho" icon={ajuda} />
-                            <Button title="Sair" classeAdicional="CadastroPaciente-botoes-cabecalho" icon={sair}/>
-                        </div>
-                    </div>
-                    <hr className="CadastroPaciente-divisao-conteudo"></hr>
-                    <Button title="Voltar" classeAdicional="CadastroPaciente-botao-voltar" icon={voltar}/>
-                </div>
-    
-
-                    <Search />
-                    
-                    <div className='treinoModal'>
-                      <h1>Treino A</h1>
+    for (let i = 0; i < quantidadeTreinos; i++) {
+      console.log('indice s', indiceSessao)
+      divsTreino.push(
+        <div key={i} className='treinoModal'>
+          
+            <h3>Treino {String.fromCharCode(65 + i)}</h3>
 
                       <div className='CadastroTreino-campos-formulario CadastroTreino-container'>
                         <div className="CadastroTreino-item-formulario">
@@ -276,14 +128,201 @@ const diasDaSemana = [
                             </div>
                             <div className='CadastroTreino-botoes-exercicios'>
                                 <Button title="Adicionar Exercício" classeAdicional="CadastroTreino-botao-opcao" onClick={e => adicionarExercicio(e, indiceExercicio)}/>
-                                <Button title="Remover Exercício" classeAdicional="CadastroTreino-botao-opcao" onClick={e => removerExercicio(e)}/>
+                                 <Button title="Remover Exercício" classeAdicional="CadastroTreino-botao-opcao" onClick={e => removerExercicio(e)}/>
                             </div>
                           </div>
                         </div>
-                      </div>
                    
+          
+        </div>
+      );
+    }
+
+    return divsTreino;
+  };
+
+  const preencheAtributoExercicio = (event, indice) => {
+    const { name, value } = event.target;
     
-                    <Button title="Continuar" classeAdicional="CadastroPaciente-enviar" icon={salvar} onClick={e => cadastrarTreino(e)}/>
+    const exercicioAtual = dadosTreino.sessoes[0].exercicios[indice]
+    exercicioAtual[name] = value;
+};
+
+  function adicionarExercicio(event, index) {
+    event.preventDefault()
+
+    dadosTreino.sessoes[0].exercicios.push({
+      nome: '',
+      series: 0,
+      repeticoes: 0,
+      carga: 0,
+      tecnicaAvancada: '',
+  })
+
+  let containerCampos = document.createElement('div')
+  containerCampos.classList.add('CadastroTreino-campos-exercicio');
+  containerCampos.dataset.index = indiceExercicio;
+
+    containerCampos.innerHTML = `
+        <div class="CadastroTreino-item-formulario-exercicio">
+            <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="nomeExercicio">Nome do Exercício</label>
+            <input class="CadastroTreino-input CadastroTreino-input-exercicio" type="text" id="nomeExercicio" name="nome" required="required" maxLength="200"/>
+        </div>
+        <div class="CadastroTreino-item-formulario-exercicio">
+            <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="series">Séries</label>
+            <input class="CadastroTreino-input CadastroTreino-input-series" type="tel" id="series" name="series" required="required" max="10" min="1" defaultValue="1"/>
+        </div>
+        <div class="CadastroTreino-item-formulario-exercicio">
+            <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="repeticoes">Repetições</label>
+            <input class="CadastroTreino-input CadastroTreino-input-repeticoes" type="tel" id="repeticoes" name="repeticoes" required="required" max="200" min="1" defaultValue="10"/>
+        </div>
+        <div class="CadastroTreino-item-formulario-exercicio">
+          <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="carga">Carga (kg)</label>
+          <input class="CadastroTreino-input CadastroTreino-input-carga" type="tel" id="carga" name="carga" required="required" max="200" min="1" defaultValue="10"/>
+        </div>
+        <div class="CadastroTreino-item-formulario-exercicio">
+          <label class="CadastroTreino-label-campo CadastroTreino-label-exercicio CadastroTreino-campo-obrigatorio" for="carga">Técnica avançada</label>
+          <input class="CadastroTreino-input CadastroTreino-input-tecavancada" type="text" id="tecavancada" name="tecnicaAvancada" required="required" max="200" min="1" defaultValue="10"/>
+        </div>
+
+    `;
+
+    const inputNome = containerCampos.querySelector('.CadastroTreino-input-exercicio');
+    inputNome.addEventListener('change', function(e) {
+        preencheAtributoExercicio(e, containerCampos.dataset.index)
+    })
+
+    const inputSeries = containerCampos.querySelector('.CadastroTreino-input-series');
+    inputSeries.addEventListener('change', function(e) {
+        preencheAtributoExercicio(e, containerCampos.dataset.index)
+    })
+
+    const inputRepeticoes = containerCampos.querySelector('.CadastroTreino-input-repeticoes');
+    inputRepeticoes.addEventListener('change', function(e) {
+        preencheAtributoExercicio(e, containerCampos.dataset.index)
+    })
+
+    const inputCarga = containerCampos.querySelector('.CadastroTreino-input-carga');
+    inputCarga.addEventListener('change', function(e) {
+        preencheAtributoExercicio(e, containerCampos.dataset.index)
+    })
+
+    const inputTecAvancada = containerCampos.querySelector('.CadastroTreino-input-tecavancada');
+    inputTecAvancada.addEventListener('change', function(e) {
+        preencheAtributoExercicio(e, containerCampos.dataset.index)
+    })
+
+    document.getElementById('formExercicio').appendChild(containerCampos);
+    indiceExercicio++;
+  }
+
+  function removerExercicio(event) {
+    event.preventDefault()
+
+    dadosTreino.sessoes[0].exercicios.pop()
+    const formExercicio = document.getElementById('formExercicio');
+    const exercicios = formExercicio.querySelectorAll('.CadastroTreino-campos-exercicio');
+
+    if (exercicios.length > 0) {
+        formExercicio.removeChild(exercicios[exercicios.length - 1]);
+    }
+
+    indiceExercicio--;
+  } 
+
+  function cadastrarTreino(event){
+    event.preventDefault();
+
+    const url = 'http://localhost:3000/treinos/656fe262751bb37143d35513';
+    console.log(dadosTreino)
+
+    axios.post(url, dadosTreino)
+      .then((response) => {
+        alert('Treino Cadastrado com sucesso!')
+
+      }, (error) => {
+        alert('Não foi possível adicionar o treino. Verifique os dados informados!')
+        console.log(error.response.data)
+      });
+  };
+
+  const preencheAtributoTreino = (event) => {
+    let { name, value } = event.target;
+
+    dadosTreino.sessoes[0][name] = value;
+};
+
+
+const diasDaSemana = [
+  'Domingo',
+  'Segunda-Feira',
+  'Terça-Feira',
+  'Quarta-Feira',
+  'Quinta-Feira',
+  'Sexta-Feira',
+  'Sábado',
+];
+
+    return(     
+        
+        <div className="CadastroPaciente">
+            <div className="CadastroPaciente-menu-lateral">
+                <Logo />
+                <span className="CadastroPaciente-menu-titulo">MENU</span>
+                <hr className="CadastroPaciente-menu-divisao" />
+                <nav className="CadastroPaciente-navegacao">
+                    <a href="/" className='CadastroPaciente-item-menu'>
+                        <MenuButton title="Pacientes" icon={paciente}/>
+                    </a>
+                    <a href="/" className='CadastroPaciente-item-menu'>
+                        <MenuButton title="Cadastrar Paciente" icon={adicaoPaciente} />
+                    </a>
+                    <a href="/" className='CadastroPaciente-item-menu'>
+                        <MenuButton title="Criar Dieta" icon={dieta}/>
+                    </a>
+                    <a href="/cadastroTreino" className='CadastroPaciente-item-menu'>
+                        <MenuButton title="Criar Plano de Treino" icon={treino} selecionado="true"/>
+                    </a>
+                    <a href="/" className='CadastroPaciente-item-menu'>
+                        <MenuButton title="Agendar Consulta" icon={agendamento} />
+                    </a>
+                </nav>
+            </div>
+    
+            <div className="CadastroPaciente-conteudo">
+                <div className="CadastroPaciente-cabecalho">
+                    <div className='CadastroPaciente-items-cabecalho'>
+                        <Header title="Criar Plano de Treino" caminhoImagem={treino} />
+                        <div className='CadastroPaciente-botoes-cabecalho'>
+                            <Button title="Ajuda" classeAdicional="CadastroPaciente-botoes-cabecalho" icon={ajuda} />
+                            <Button title="Sair" classeAdicional="CadastroPaciente-botoes-cabecalho" icon={sair}/>
+                        </div>
+                    </div>
+                    <hr className="CadastroPaciente-divisao-conteudo"></hr>
+                    <Button title="Voltar" classeAdicional="CadastroPaciente-botao-voltar" icon={voltar}/>
+                </div>
+    
+
+                    <Search />
+
+            <div className='form-gera-treino'>
+            <label className="CadastroTreino-label-campo CadastroTreino-campo-obrigatorio" htmlFor="quantidadeTreinos">
+                  Quantidade de Treinos
+                </label>
+                <input
+                  className='CadastroTreino-input CadastroTreino-input-numero-treinos'
+                  type="tel"
+                  id="quantidadeTreinos"
+                  value={quantidadeTreinos}
+                  onChange={e => handleChangeQuantidadeTreinos(e)}
+                />
+            </div>
+
+                    {/* Renderiza as divs de treino com base na quantidade */}
+                    {gerarDivsTreino()
+                    }
+
+                    <Button title="Finalizar" classeAdicional="CadastroPaciente-enviar" icon={salvar} onClick={e => cadastrarTreino(e)}/>
 
             </div>
             <Footer className="CadastroPaciente-rodape"/>

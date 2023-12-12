f13bc15641f04770e4327082fa0c1b4540403f4a
Lucas de Lima Spinosa <lucasspinosa.software@gmail.com>
2023-12-12 02:54:30 -0300

    refact (cadastro de paciente): restauração de correções

--- frontend/app/src/Pages/CadastroPaciente/CadastroPaciente.jsx
+++ frontend/app/src/Pages/CadastroPaciente/CadastroPaciente.jsx
@@ -34,28 +34,24 @@ const CadastroPaciente = () =>{
             dtNascimento: '',
             sexo: '',
             email: '',
-            telefone: '',
-            endereco: '',
+            //telefone: '',
+            //endereco: '',
             questionario: {
                 objetivosPaciente: '',
-                problemasSaudeIndividual: '',
-                problemasSaudeFamiliares: '',
-                medicamentosIngeridos: [{
-                    nome: '',
-                    horario: '',
-                    tipo: '',
-                }],
-                alergiasAlimentares: '',
-                alteracoes: '',
-                cirurgias: '',
-                lesoes: '',
-                restricoesMedicas: '',
-                habitosGeraisAlimentares: ''
+              //problemasSaudeIndividual: '',
+              //problemasSaudeFamiliares: '',
+                medicamentosIngeridos: [],
+                //alergiasAlimentares: '',
+                //alteracoes: '',
+                //cirurgias: '',
+                //lesoes: '',
+                //restricoesMedicas: '',
+                //habitosGeraisAlimentares: ''
             },
             gastoEnergeticoDiario: '',
             metabolismoBasal: '',
             valorCaloricoPlano: '',
-            conclusoes: ''
+            //conclusoes: ''
         },
         medida: {
             altura: '',
